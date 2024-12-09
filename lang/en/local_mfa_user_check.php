7cafd42393895c4da21893e5975dd027d1fda36b
vadikonline1 <vadikonline1@gmail.com>
2024-12-09 12:18:50 +0200

    Create local_mfa_user_check.php

---
+++ lang/en/local_mfa_user_check.php
@@ -0,0 +1,11 @@
+<?php
+$string['pluginname'] = 'MFA User Check';
+$string['tool_mfa_status'] = 'MFA Status';
+$string['tool_mfa_active'] = 'General MFA (tool_mfa) : is active';
+$string['tool_mfa_inactive'] = 'General MFA (tool_mfa) : is inactive';
+$string['pluginenabled'] = 'Enable MFA User Check Plugin';
+$string['pluginenabled_desc'] = 'Check this box to enable the MFA User Check Plugin.';
+$string['redirecturl'] = 'Redirect URL';
+$string['redirecturl_desc'] = 'Value redirect';
+$string['exclude_urls'] = 'Excluded URLs';
+$string['exclude_urls_list'] = 'Enter the URLs that should be excluded, one per line. Not include: /admin/tool/mfa/action.php /admin/tool/mfa/guide.php';  
