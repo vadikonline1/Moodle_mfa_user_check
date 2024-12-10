cec840e1c6bb6189769223ee08e1f245ef8cf266
vadikonline1 <vadikonline1@gmail.com>
2024-12-10 15:44:31 +0200

    Update lib.php

--- lib.php
+++ lib.php
@@ -7,25 +7,26 @@ function local_mfa_user_check_extend_navigation_user($user) {
     if (!$plugin_enabled) {
         return;
     }
+	// Verificăm dacă utilizatorul este în lista albă
+    $whitelist_ids = get_config('local_mfa_user_check', 'whitelist_users_id');
+    // Dacă nu există setare pentru whitelist, atribuim un ID implicit (ID-ul 2)
+    if (empty($whitelist_ids)) {
+        $whitelist_ids = '2'; // ID-ul implicit
+    }
+    $whitelist_ids = explode(',', $whitelist_ids); // Împărțim ID-urile utilizatorilor
 
-    if ($USER->id == 2) {
+    if (in_array($USER->id, $whitelist_ids)) {
         return;
     }
 
     if (isloggedin() && !isguestuser()) {
-        $redirect_url = get_config('local_mfa_user_check', 'redirect_url') ?: '/admin/tool/mfa/user_preferences.php';
+        $redirect_url = '/admin/tool/mfa/user_preferences.php';
         $current_url = $PAGE->url->out();
+		$exclude_urls = [
+			$redirect_url,
+			'/admin/tool/mfa/'
+			];
 
-        $exclude_urls_config = get_config('local_mfa_user_check', 'exclude_urls');
-        if (!empty($exclude_urls_config)) {
-            $exclude_urls = array_map('trim', explode(',', $exclude_urls_config));
-        } else {
-            $exclude_urls = [
-                $redirect_url,
-                '/admin/tool/mfa/action.php',
-                '/admin/tool/mfa/guide.php'
-            ];
-        }
 
 		foreach ($exclude_urls as $url) {
 			if (strpos($current_url, $url) !== false) {
@@ -33,9 +34,10 @@ function local_mfa_user_check_extend_navigation_user($user) {
 			}
 		}
 
-        $result = $DB->get_record('tool_mfa', array('userid' => $USER->id, 'factor' => 'totp'));
-        if (!$result || $result->revoked == 1) {
-            redirect(new moodle_url($redirect_url));
+        $result = $DB->get_records('tool_mfa', array('userid' => $USER->id, 'factor' => 'totp', 'revoked' => 0));
+        if ($result) {
+			return;
         }
+		redirect(new moodle_url($redirect_url));
     }
 }
